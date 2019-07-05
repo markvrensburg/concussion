0c1bac9b9aa19a97307af0935b0fe86fec835bfa
Mark van Rensburg <jansevanrensburg.mark@gmail.com>
2019-07-05 20:23:25 +0200

    Working bezier connector

--- frontend/src/main/scala/concussion/component/editor/Connector.scala
+++ frontend/src/main/scala/concussion/component/editor/Connector.scala
@@ -5,7 +5,7 @@ import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.component.Scala.Unmounted
 import scalatags.Text.svgAttrs._
-import scalatags.Text.svgTags._
+import scalatags.Text.svgTags.{circle, _}
 import scalatags.Text.implicits._
 import scalacss.ScalaCssReact._
 
@@ -15,47 +15,151 @@ object Connector {
   private val strokeWidth: Double = 2
   private val bezierWeight = 0.5
 
-  private def clamp(min: Double, max: Double)(value: Double) =
-    Math.max(min, Math.min(max, value))
+  final case class Point(x: Double, y: Double) {
+
+    def add(point: Point): Point = Point(x + point.x, y + point.y)
+
+    def +(point: Point): Point = add(point)
+
+    def sub(point: Point): Point = Point(x - point.x, y - point.y)
+
+    def -(point: Point): Point = sub(point)
+
+    def mul(scalar: Double): Point = Point(scalar * x, scalar * y)
+
+    def mul(point: Point): Point = Point(x * point.x, y * point.y)
+
+    def *(scalar: Double): Point = mul(scalar)
+
+    def *(point: Point): Point = mul(point)
+
+    def maxByX(point: Point): Point = if (x > point.x) this else point
+
+    def maxByY(point: Point): Point = if (y > point.y) this else point
+
+    def minByX(point: Point): Point = if (x <= point.x) this else point
+
+    def minByY(point: Point): Point = if (y <= point.y) this else point
+
+    def max(point: Point): Point = Point(maxByX(point).x, maxByY(point).y)
+
+    def min(point: Point): Point = Point(minByX(point).x, minByY(point).y)
+
+  }
+
+  final case class Rect(topLeft: Point, bottomRight: Point) {
+
+    lazy val height: Double = bottomRight.y - topLeft.y
+
+    lazy val width: Double = bottomRight.x - topLeft.x
+  }
 
   final case class Props(from: Port, to: Port, dashed: Boolean) {
 
-    private val orderPortX = Vector(from, to).sortBy(_.x)
-    private val orderPortY = Vector(from, to).sortBy(_.y)
+    lazy val p0: Point = Point(from.x, from.y)
 
-    val left: Port = orderPortX.head
-    val right: Port = orderPortX(1)
+    lazy val p3: Point = Point(to.x, to.y)
 
-    val top: Port = orderPortY.head
-    val bottom: Port = orderPortY(1)
+    lazy val dx: Double = Math.min(500, Math.abs(from.x - to.x) * bezierWeight)
 
-    val height: Double = bottom.y - top.y
-    val width: Double = right.x - left.x
+    lazy val p1: Point = from.orientation match {
+      case Right => Point(p0.x + dx, p0.y)
+      case Left  => Point(p0.x - dx, p0.y)
+      case _     => Point(p0.x, p0.y)
+    }
 
-    def connector: String = {
+    lazy val p2: Point = to.orientation match {
+      case Right => Point(p3.x + dx, p3.y)
+      case Left  => Point(p3.x - dx, p3.y)
+      case _     => Point(p3.x, p3.y)
+    }
 
-      val x_1 = radius
-      val x_2 = radius + width
+    lazy val boundingBox: Rect = {
+      var mi = p0.min(p3)
+      var ma = p0.max(p3)
 
-      val (y_1, y_2) = (left.y, right.y) match {
-        case (topY, botY) if topY < botY => (radius, radius + height)
-        case _                           => (radius + height, radius)
+      val c = p1 - p0 //-1.0 * p0 + 1.0 * p1;
+      val b = p0 - (p1 * 2) + p2 //1.0*p0 - 2.0*p1 + 1.0*p2;
+      val a = (p1 * 3) - p0 - (p2 * 3) + p3 //-1.0*p0 + 3.0*p1 - 3.0*p2 + 1.0*p3;
+
+      var h = (b * b) - (a * c)
+
+      @inline def q_(s: Double, t: Double, v1: Double, v2: Double, v3: Double, v4: Double) =
+        (s * s * s * v1) + (3.0 * s * s * t * v2) + (3.0 * s * t * t * v3) + (t * t * t * v4)
+
+      if (h.x > 0.0) {
+        h = h.copy(x = Math.sqrt(h.x))
+        var t = c.x / (-b.x - h.x)
+        if (t > 0.0 && t < 1.0) {
+          val s = 1.0 - t
+          val q = q_(s, t, p0.x, p1.x, p2.x, p3.x)
+          mi = mi.copy(x = Math.min(mi.x, q))
+          ma = ma.copy(x = Math.max(ma.x, q))
+        }
+        t = c.x / (-b.x + h.x)
+        if (t > 0.0 && t < 1.0) {
+          val s = 1.0 - t
+          val q = q_(s, t, p0.x, p1.x, p2.x, p3.x)
+          mi = mi.copy(x = Math.min(mi.x, q))
+          ma = ma.copy(x = Math.max(ma.x, q))
+        }
+      }
+      if (h.y > 0.0) {
+        h = h.copy(y = Math.sqrt(h.y))
+        var t = c.x / (-b.y - h.y)
+        if (t > 0.0 && t < 1.0) {
+          val s = 1.0 - t
+          val q = q_(s, t, p0.y, p1.y, p2.y, p3.y)
+          mi = mi.copy(y = Math.min(mi.y, q))
+          ma = ma.copy(y = Math.max(ma.y, q))
+        }
+        t = c.x / (-b.y + h.y)
+        if (t > 0.0 && t < 1.0) {
+          val s = 1.0 - t
+          val q = q_(s, t, p0.y, p1.y, p2.y, p3.y)
+          mi = mi.copy(y = Math.min(mi.y, q))
+          ma = ma.copy(y = Math.max(ma.y, q))
+        }
       }
 
-      val dx = clamp(50, 500)(width * bezierWeight)
-      val c1 = from.orientation match {
-        case Right => x_1 + dx
-        case Left  => x_1 - dx
-        case _     => x_1
-      }
+      val box = Rect(mi, ma)
 
-      val c2 = to.orientation match {
-        case Right => x_2 + dx
-        case Left  => x_2 - dx
-        case _     => x_2
-      }
+      val xLeft =
+        if ((Math.min(p0.x, p3.x) - box.topLeft.x) <= radius)
+          box.topLeft.x - radius
+        else
+          box.topLeft.x - strokeWidth
 
-      val connectorPath = s"M$x_1 $y_1 C $c1 $y_1 $c2 $y_2 $x_2 $y_2"
+      val xRight =
+        if ((box.bottomRight.x - Math.max(p0.x, p3.x)) <= radius)
+          box.bottomRight.x + radius
+        else
+          box.bottomRight.x + strokeWidth
+
+      val yTop = Math.min(mi.y, ma.y) - radius
+      val yBot = Math.max(mi.y, ma.y) + radius
+
+      Rect(Point(xLeft, yTop), Point(xRight, yBot))
+    }
+
+    def offset(point: Point) =
+      Point(point.x - boundingBox.topLeft.x, point.y - boundingBox.topLeft.y)
+
+    lazy val connector: String = {
+
+      val p0Offset = offset(p0)
+      val p1Offset = offset(p1)
+      val p2Offset = offset(p2)
+      val p3Offset = offset(p3)
+
+      val x_1 = p0Offset.x
+      val y_1 = p0Offset.y
+      val x_2 = p3Offset.x
+      val y_2 = p3Offset.y
+      val c_1 = p1Offset.x
+      val c_2 = p2Offset.x
+
+      val connectorPath = s"M$x_1 $y_1 C $c_1 $y_1 $c_2 $y_2 $x_2 $y_2"
 
       val dashes = if (dashed) "stroke-dasharray: 4" else ""
 
@@ -78,10 +182,10 @@ object Connector {
       .render_P(
         p =>
           <.div(
-            ^.left := s"${p.left.x - radius}",
-            ^.top := s"${p.top.y - radius}",
-            ^.height := s"${p.height + (2 * radius)}",
-            ^.width := s"${p.width + (2 * radius)}", //todo take into account bezier curve width
+            ^.left := s"${p.boundingBox.topLeft.x}",
+            ^.top := s"${p.boundingBox.topLeft.y}",
+            ^.height := s"${p.boundingBox.height}",
+            ^.width := s"${p.boundingBox.width}",
             ^.position := "absolute",
             ConnectorStyle.connector,
             ^.dangerouslySetInnerHtml := p.connector
